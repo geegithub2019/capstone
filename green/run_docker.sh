544602ac522239527b20eb56e67a15364a017c3e
root <root@ip-10-4-0-61.us-west-2.compute.internal>
2020-01-25 06:33:20 +0000

    Green image 5

--- green/run_docker.sh
+++ green/run_docker.sh
@@ -8,8 +8,8 @@ docker build --tag=greenimage .
 
 # Step 2: 
 # List docker images
-docker image ls
+#docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 greenimage
+#docker run -p 8000:80 greenimage
