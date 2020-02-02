8014d3712ff8a4c30b032dd49072526aeef8b44c
Ubuntu <ubuntu@ip-10-4-0-118.us-west-2.compute.internal>
2020-02-02 07:04:16 +0000

    Testing

--- green/run_kubernetes.sh
+++ green/run_kubernetes.sh
@@ -1,5 +1,9 @@
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl apply -f ./green/deployment.yml
-kubectl apply -f ./green/service.yml
+#kubectl apply -f ./green/deployment.yml
+#kubectl apply -f ./green/service.yml
+
+kubectl create deployment greenimage --image=geepee2017/greenimage
+
+
